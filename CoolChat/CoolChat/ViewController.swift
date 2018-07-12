c28640c012e12b9b71a958caace80306e6e1115b
Kaderabkova <KKD@hudadmins-MacBook-Pro.local>
2018-07-12 14:08:11 +0200

    Zalozeni XCode projektu

---
+++ CoolChat/CoolChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolChat
+//
+//  Created by Kaderabkova on 12/07/2018.
+//  Copyright © 2018 ICT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
